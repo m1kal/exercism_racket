d867cfc29155529aa137edc6c26bf5808647fcc7
m1kal <mikal82@protonmail.com>
2018-10-05 10:33:34 +0200

    Leap v3

--- leap/leap.rkt
+++ leap/leap.rkt
@@ -4,8 +4,8 @@
 
 (define (multiple? n m) (zero? (modulo n m)))
 
+(define (comb r1 r2 r3) (and r3 (not (xor r1 r2))))
+
 (define (leap-year? year)
   (let ([year-multiple-of? (lambda (n) (multiple? year n))])
-    (or
-      (and (year-multiple-of? 4) (not (year-multiple-of? 100)))
-      (year-multiple-of? 400))))
+    (apply comb (map year-multiple-of? '(400 100 4)))))
