46037fff3a63caf9872bf101d3ed9a8067c17390
m1kal <mikal82@protonmail.com>
2018-11-03 21:09:29 +0100

    Perfect-numbers v4

--- perfect-numbers/perfect-numbers.rkt
+++ perfect-numbers/perfect-numbers.rkt
@@ -3,20 +3,12 @@
 (provide perfect-numbers)
 
 (define (perfect-numbers limit)
-  (for/list ([number (range-base1 limit)]
+  (for/list ([number  (in-range 1 (add1 limit))]
              #:when (perfect? number))
     number))
 
 (define (perfect? number)
-  (= number (divisors number)))
-
-(define (divisors number)
-  (for/sum ([value (range-base1 (/ (sub1 number) 2))]
-            #:when ((divisor? number) value))
-    value))
-
-(define (divisor? number)
-  (lambda (val) (zero? (modulo number val))))
-
-(define (range-base1 number)
-  (in-range 1 (add1 number)))
+  (= number
+    (for/sum ([value (in-range 1 (add1 (/ (sub1 number) 2)))]
+              #:when (zero? (modulo number value)))
+      value)))
