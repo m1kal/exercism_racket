3941476fba59fbbf8005f0ff59b7f89ae0cb16f4
m1kal <mikal82@protonmail.com>
2018-11-15 13:02:38 +0100

    Hamming v2

--- hamming/hamming.rkt
+++ hamming/hamming.rkt
@@ -3,7 +3,7 @@
 (provide hamming-distance)
 
 (define (hamming-distance s1 s2)
-  (cond ((not (apply = (map string-length (list s1 s2))))
-         (raise-argument-error 'hamming-distance "Equal string length")))
+  (when (not (apply = (map string-length (list s1 s2))))
+        (raise-argument-error 'hamming-distance "Equal string length"))
   (for/sum [[char1 s1] [char2 s2] #:when (not (char=? char1 char2))] 1))
 
